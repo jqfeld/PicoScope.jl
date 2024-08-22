cc61e78045ba80789e98a9290096ad69e13812db
Documenter.jl <documenter@juliadocs.github.io>
2024-08-22 04:00:27 +0000

    build based on 4049be8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PicoScope","category":"page"},{"location":"#PicoScope","page":"Home","title":"PicoScope","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PicoScope.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PicoScope]","category":"page"}]
+}
